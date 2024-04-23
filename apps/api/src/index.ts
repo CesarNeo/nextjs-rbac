938ce40f0f3dd8ef2195e38d43394389b956be8a
Cesar Emmanuel <cesaremmmanul@gmail.com>
2024-04-23 17:41:55 -0300

    feat: create api using fastify and routes

--- apps/api/src/index.ts
+++
@@ -1,9 +0,0 @@
-import { defineAbilityFor } from '@neo-saas/auth'
-import { projectSchema } from '@neo-saas/auth/src/models'
-
-const ability = defineAbilityFor({ role: 'MEMBER', id: 'user-id' })
-const project = projectSchema.parse({ id: 'project-id', ownerId: 'user-id' })
-
-console.log(ability.can('get', 'Billing'))
-console.log(ability.can('create', 'Invite'))
-console.log(ability.can('delete', project))
