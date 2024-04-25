6ec0a890a366456148cc3330ca25c31eb5223bf8
Cesar Emmanuel <cesaremmmanul@gmail.com>
2024-04-25 15:30:25 -0300

    feat: create error handling with fastify

--- apps/api/src/http/routes/auth/authenticate-with-password.ts
+++ apps/api/src/http/routes/auth/authenticate-with-password.ts
@@ -5,6 +5,8 @@ import z from 'zod'
 
 import { prisma } from '@/lib/prisma'
 
+import { BadRequestError } from '../_errors/bad-request-error'
+
 export async function authenticateWithPassword(app: FastifyInstance) {
   app.withTypeProvider<ZodTypeProvider>().post(
     '/sessions/password',
@@ -16,10 +18,7 @@ export async function authenticateWithPassword(app: FastifyInstance) {
           email: z.string().email(),
           password: z.string().min(6),
         }),
-        response: {
-          400: z.object({ message: z.string() }),
-          201: z.object({ token: z.string() }),
-        },
+        response: { 201: z.object({ token: z.string() }) },
       },
     },
     async (request, reply) => {
@@ -30,13 +29,13 @@ export async function authenticateWithPassword(app: FastifyInstance) {
       })
 
       if (!userFromEmail) {
-        return reply.status(400).send({ message: 'Invalid credentials.' })
+        throw new BadRequestError('Invalid credentials.')
       }
 
       if (userFromEmail.passwordHash === null) {
-        return reply
-          .status(400)
-          .send({ message: 'User does not have a password, use social login.' })
+        throw new BadRequestError(
+          'User does not have a password, use social login.',
+        )
       }
 
       const isValidPassword = await compare(
@@ -45,7 +44,7 @@ export async function authenticateWithPassword(app: FastifyInstance) {
       )
 
       if (!isValidPassword) {
-        return reply.status(400).send({ message: 'Invalid credentials.' })
+        throw new BadRequestError('Invalid credentials.')
       }
 
       const token = await reply.jwtSign(
