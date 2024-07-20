b056095da9947cffc6e3d5f224d01aa061e787c3
Cesar Emmanuel <cesaremmmanul@gmail.com>
2024-07-20 12:50:53 -0300

    feat: create pages register/recover

---
+++ apps/web/src/app/auth/forgot-password/page.tsx
@@ -0,0 +1,26 @@
+import Link from 'next/link'
+
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+
+function ForgotPasswordPage() {
+  return (
+    <form className="space-y-4">
+      <div className="space-y-1">
+        <Label htmlFor="email">E-mail</Label>
+        <Input id="email" type="email" name="email" />
+      </div>
+
+      <Button className="w-full" type="submit">
+        Recover password
+      </Button>
+
+      <Button className="w-full" variant="link" asChild size="sm">
+        <Link href="/auth/sign-in">Sign in instead</Link>
+      </Button>
+    </form>
+  )
+}
+
+export default ForgotPasswordPage
--- apps/web/src/app/auth/sign-up/page.tsx
+++ apps/web/src/app/auth/sign-up/page.tsx
@@ -7,9 +7,14 @@ import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 import { Separator } from '@/components/ui/separator'
 
-function SignInPage() {
+function SignUpPage() {
   return (
     <form className="space-y-4">
+      <div className="space-y-1">
+        <Label htmlFor="name">Name</Label>
+        <Input id="name" name="name" />
+      </div>
+
       <div className="space-y-1">
         <Label htmlFor="email">E-mail</Label>
         <Input id="email" type="email" name="email" />
@@ -18,18 +23,23 @@ function SignInPage() {
       <div className="space-y-1">
         <Label htmlFor="password">Password</Label>
         <Input id="password" type="password" name="password" />
+      </div>
 
-        <Button variant="link" size="sm" asChild className="pl-0">
-          <Link href="/auth/forgot-password">Forgot our password?</Link>
-        </Button>
+      <div className="space-y-1">
+        <Label htmlFor="password_confirmation">Confirm your password</Label>
+        <Input
+          id="password_confirmation"
+          type="password"
+          name="password_confirmation"
+        />
       </div>
 
       <Button className="w-full" type="submit">
-        Sign in with e-mail
+        Create account
       </Button>
 
       <Button className="w-full" variant="link" asChild size="sm">
-        <Link href="/auth/sign-up">Create new account</Link>
+        <Link href="/auth/sign-in">Already registered? Sign in</Link>
       </Button>
 
       <Separator />
@@ -42,10 +52,10 @@ function SignInPage() {
           height={16}
           className="mr-2 size-4 dark:invert"
         />
-        Sign in with Github
+        Sign up with Github
       </Button>
     </form>
   )
 }
 
-export default SignInPage
+export default SignUpPage
