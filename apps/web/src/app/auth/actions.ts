6b71ebb94791b65084dc49d298ebebbe8e53876d
Cesar Emmanuel <cesaremmmanul@gmail.com>
2024-07-21 12:15:23 -0300

    feat: using envs in project

--- apps/web/src/app/auth/actions.ts
+++ apps/web/src/app/auth/actions.ts
@@ -1,14 +1,14 @@
 'use server'
 
+import { env } from '@neo-saas/env'
 import { redirect } from 'next/navigation'
 
 export async function signInWithGithub() {
+  const { GITHUB_OAUTH_CLIENT_ID, GITHUB_OAUTH_REDIRECT_URI } = env
+
   const githubSignInURL = new URL('login/oauth/authorize', 'https://github.com')
-  githubSignInURL.searchParams.set('client_id', '1754f5db2e0d1813cfb7')
-  githubSignInURL.searchParams.set(
-    'redirect_uri',
-    'http://localhost:3000/api/auth/callback',
-  )
+  githubSignInURL.searchParams.set('client_id', GITHUB_OAUTH_CLIENT_ID)
+  githubSignInURL.searchParams.set('redirect_uri', GITHUB_OAUTH_REDIRECT_URI)
   githubSignInURL.searchParams.set('scope', 'user')
 
   return redirect(String(githubSignInURL))
