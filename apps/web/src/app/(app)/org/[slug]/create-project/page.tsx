370b3e7514b420d9e1b875f60914ecebe4cf14fe
Cesar Emmanuel <cesaremmmanul@gmail.com>
2024-07-22 20:44:27 -0300

    feat: verify permission to create project

--- apps/web/src/app/(app)/org/[slug]/create-project/page.tsx
+++ apps/web/src/app/(app)/org/[slug]/create-project/page.tsx
@@ -1,8 +1,18 @@
+import { redirect } from 'next/navigation'
+
+import { ability } from '@/auth/auth'
 import Header from '@/components/header'
+import { RoutesPath } from '@/enums/routes-path'
 
 import ProjectForm from './project-form'
 
-function CreateProjectPage() {
+async function CreateProjectPage() {
+  const permissions = await ability()
+
+  if (permissions?.cannot('create', 'Project')) {
+    return redirect(RoutesPath.HOME)
+  }
+
   return (
     <div className="space-y-4 py-4">
       <Header />
